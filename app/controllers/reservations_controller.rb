55beb9aedd7bf0eff374b7dff6b89df94b8aaf9b
benjibennn <benjaminwong94@hotmail.my>
2018-05-04 18:40:20 +0800

    background video on homepage added

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -8,8 +8,7 @@ class ReservationsController < ApplicationController
 	 	reservation.user_id = current_user.id
 	 	if reservation.save
 	 		ReservationJob.perform_later(current_user,reservation.listing_id,reservation.id)
-	 		# ReservationMailer.booking_email(current_user,reservation.listing_id,reservation.id).deliver_now
-	 		redirect_to '/'
+	 		redirect_to '/braintree/new'
 	 	else
 	 		flash[:overlap] = "Dates are already booked!"
 	 		redirect_to new_listing_reservation_path(params[:listing_id])
